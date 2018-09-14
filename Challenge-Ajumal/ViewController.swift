0597da53e26542366ec953a53a9d0fffe4710589
Ajumal <aebrahim@talabat.com>
2018-09-14 19:05:11 +0300

    Initial Commit

---
+++ Challenge-Ajumal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Challenge-Ajumal
+//
+//  Created by Ajumal Ebrahim on 9/14/18.
+//  Copyright © 2018 Ajumal Ebrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
