e68c7d6c7c6d8c7eee06f9722901377a513fbac4
Ajumal <aebrahim@talabat.com>
2018-09-15 18:31:04 +0300

    Initiated the first view

--- Challenge-Ajumal/AppDelegate.swift
+++ Challenge-Ajumal/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let view = ViewController()
+        let nav = UINavigationController.init(rootViewController: view)
+        window!.rootViewController = nav
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
--- Challenge-Ajumal/ViewController.swift
+++ Challenge-Ajumal/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        title = "Things to Deliver"
     }
 
     override func didReceiveMemoryWarning() {
